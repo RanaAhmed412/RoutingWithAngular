c28392776f86510d813a746019c1d5da28231e8d
Rana Ahmed <116879537+RanaAhmed412@users.noreply.github.com>
2025-01-26 16:33:27 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactComponent } from "../contact/contact.component";
+import { AboutComponent } from "../about/about.component";
+import { HomeComponent } from "../home/home.component";
+import { PortfolioComponent } from "../portfolio/portfolio.component";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactComponent, AboutComponent, HomeComponent, PortfolioComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
